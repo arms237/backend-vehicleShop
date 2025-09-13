16fea83e842ad2ab4d9934f6b7247bfb421e9d12
arms237 <134302560+arms237@users.noreply.github.com>
2025-09-13 15:33:27 +0100

    vehicle shop init

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {I18nModule, QueryResolver, HeaderResolver} from 'nestjs-i18n';
+import * as path from 'path';
+
+@Module({
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] }, // ?lang=fr
+        new HeaderResolver(['x-lang']), // Header: x-lang: jp
+      ],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
